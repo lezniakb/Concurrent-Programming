916f476211aada09d8e7f40e2e43bfe55612e19f
Bartosz <251574@edu.p.lodz.pl>
2025-05-10 14:59:15 +0200

    Nowe unit testy do DataImplementation

--- CP/DataTest/DataImplementationUnitTest.cs
+++ CP/DataTest/DataImplementationUnitTest.cs
@@ -8,6 +8,11 @@
 //
 //_____________________________________________________________________________________________________________________________________
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
 namespace TP.ConcurrentProgramming.Data.Test
 {
     [TestClass]
@@ -64,8 +69,8 @@ namespace TP.ConcurrentProgramming.Data.Test
                 Assert.AreEqual<int>(numberOfBalls2Create, numberOfCallbackInvoked);
                 newInstance.CheckNumberOfBalls(x => Assert.AreEqual<int>(10, x));
             }
-
         }
+
         [TestMethod]
         public void VerifyBallCountAfterStart()
         {
@@ -77,6 +82,67 @@ namespace TP.ConcurrentProgramming.Data.Test
                 dataImplementation.CheckNumberOfBalls(count =>
                 {
                     Assert.AreEqual(kulki, count, "ilosc kulek nie rowna sie utworzonej ilosci");
+                });
+            }
+        }
+
+        [TestMethod]
+        public void SetScreenSizeTestMethod()
+        {
+            using (DataImplementation dataImplementation = new DataImplementation())
+            {
+                double width = 1280;
+                double height = 720;
+                dataImplementation.SetScreenSize(width, height);
+
+                int ballCount = 5;
+                List<IBall> createdBalls = new List<IBall>();
+
+                dataImplementation.Start(ballCount, (position, ball) => {
+                    createdBalls.Add(ball);
+                    Assert.IsTrue(position.x >= width * 0.1);
+                    Assert.IsTrue(position.x <= width * 0.9);
+                    Assert.IsTrue(position.y >= height * 0.1);
+                    Assert.IsTrue(position.y <= height * 0.9);
+                });
+                dataImplementation.CheckNumberOfBalls(count => Assert.AreEqual(ballCount, count));
+            }
+        }
+
+        [TestMethod]
+        public void StartWithNullHandlerThrowsException()
+        {
+            using (DataImplementation dataImplementation = new DataImplementation())
+            {
+                Assert.ThrowsException<ArgumentNullException>(() => dataImplementation.Start(5, null));
+            }
+        }
+
+        [TestMethod]
+        public void VerifyBallsMove()
+        {
+            using (DataImplementation dataImplementation = new DataImplementation())
+            {
+                dataImplementation.SetScreenSize(1000, 800);
+                Dictionary<IBall, IVector> initialPositions = new Dictionary<IBall, IVector>();
+
+                dataImplementation.Start(1, (position, ball) => {
+                    initialPositions.Add(ball, new Vector(position.x, position.y));
+                });
+                Thread.Sleep(100);
+                dataImplementation.CheckBallsList(balls => {
+                    foreach (IBall ball in balls)
+                    {
+                        IVector initialPos = initialPositions[ball];
+                        Ball concreteBall = ball as Ball;
+                        Assert.IsNotNull(concreteBall, "Ball powinno byc castowane");
+
+                        IVector currentPos = concreteBall.getPosition;
+                        Assert.IsTrue(
+                            initialPos.x != currentPos.x || initialPos.y != currentPos.y,
+                            "Kulka nie ruszyla sie ze swojej pozycji.."
+                        );
+                    }
                 });
             }
         }
